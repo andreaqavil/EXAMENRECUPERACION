195393bb1ac0335443e67500cc97103d26be9857
Andrea Quinde Avila <andreaqavil@gmail.com>
2016-06-03 17:26:47 -0500

    agregue

--- js/buques.js
+++ js/buques.js
@@ -1,2 +1,10 @@
-<label for = "userbuque" > buque</ label>
-}
+alert("Bienvenidos a galeria de Buques");
+
+var buque1 =
+{buque: "buque santiago"};
+var buque2 =
+{buque: "isla puna"};
+var buque3 =
+{buque: "buque zaruma"};
+var buque4=
+{buque: "panamax"};
